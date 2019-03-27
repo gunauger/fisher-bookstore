1eb8a26a3a677666f1212a19775e4d623a7d0972
gunauger <46544174+gunauger@users.noreply.github.com>
2019-03-27 15:59:20 -0400

    Add files via upload

---
+++ api/Models/Book.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Fisher.Bookstore.Models
+{
+    public class Book
+    {
+        public int Id {get;set;}
+        public string Title {get;set;}
+        public string Author {get;set;}
+        public string ISBN {get;set;}
+        public DateTime publicationDate {get;set;}
+    }
+}
